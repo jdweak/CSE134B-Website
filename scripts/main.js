96f52cc12d53559630b2d4ab80d8c4dbdd87f403
Jonathan Weakliem <67931921+jdweak@users.noreply.github.com>
2023-10-25 21:48:04 -0700

    framework for main page

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,24 @@
-/* main.js */
+// /* main.js */
 
-// no need to 'use strict' as it is implied in modules
-// look up 'use strict' :) 
+// // no need to 'use strict' as it is implied in modules
+// // look up 'use strict' :) 
 
-let $ = (selector) => document.querySelector(selector);
+// let $ = (selector) => document.querySelector(selector);
 
 
-function _updateName() {
-  const name = $('#name-input').value;
-  $('#name-info').innerHTML = `<strong>Author:</strong>  ${name}`;
-}
+// function _updateName() {
+//   const name = $('#name-input').value;
+//   $('#name-info').innerHTML = `<strong>Author:</strong>  ${name}`;
+// }
 
-function _updateMajor() {
-  const major = $('#major-input').value;
-  $('#major-info').innerHTML = `<strong>Major:</strong>  ${major}`;
-}
+// function _updateMajor() {
+//   const major = $('#major-input').value;
+//   $('#major-info').innerHTML = `<strong>Major:</strong>  ${major}`;
+// }
 
-function update(){
-  _updateName();
-  _updateMajor();
-}
+// function update(){
+//   _updateName();
+//   _updateMajor();
+// }
 
-export {update};
+// export {update};
